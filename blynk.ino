ca098dad36e10d27c574bcbbfd93e995468bc28b
ts22842 <ts22842@student.uni-lj.si>
2023-05-15 13:46:57 +0200

    Connection to blynk

---
+++ blynk.ino
@@ -0,0 +1,37 @@
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+#define BLYNK_TEMPLATE_ID ""
+#define BLYNK_TEMPLATE_NAME ""
+#define BLYNK_AUTH_TOKEN ""
+
+char ssid[] = "";
+char pass[] = "";
+
+BlynkTimer timer;
+
+void send()
+{
+    int koraki = 1;
+    float razdalja = 5;
+    float kalorije = 10;
+
+    Blynk.virtualWrite(V0, koraki);
+    Blynk.virtualWrite(V1, razdalja);
+    Blynk.virtualWrite(V2, koraki);
+}
+
+void setup()
+{
+    Serial.begin(115200);
+
+    Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+
+    timer.setInterval(1000L, send);
+}
+
+void loop()
+{
+    Blynk.run();
+    timer.run();
+}
