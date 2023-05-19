9ee5ee998bd45f193845fdda2425965187845fe6
Generalsava <zan24pockar@gmail.com>
2023-05-19 09:06:00 +0200

    Update blynk.ino
    Tip aktivnosti

--- blynk/blynk.ino
+++ blynk/blynk.ino
@@ -15,10 +15,12 @@ void send()
     int koraki = 1;
     float razdalja = 5;
     float kalorije = 10;
+    char Tip[] = "Hoja";
 
     Blynk.virtualWrite(V0, koraki);
     Blynk.virtualWrite(V1, razdalja);
-    Blynk.virtualWrite(V2, koraki);
+    Blynk.virtualWrite(V2, kalorije);
+    Blynk.virtualWrite(V3, Tip);
 }
 
 void setup()
