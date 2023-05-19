8256fee91c13c185f72b4a4849e9b8762efae37b
tomssojer <tom.sojer2@gmail.com>
2023-05-19 14:27:42 +0200

    Corrected data types

--- main/equation.h
+++ main/equation.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <float.h>
 
 #define HEIGHT 180
 #define WEIGHT 80
@@ -12,11 +13,19 @@
 class Equation
 {
 public:
+    int SIZE;
+
+    void setSize(int size)
+    {
+        SIZE = size;
+        printf("Size: %d\n", SIZE);
+    }
+
     // Returns the array of accelerations with the highest values
     float *get_highest_acc(float *x_dir, float *y_dir, float *z_dir)
     {
         int x, y, z;
-        for (int i = 0; i < sizeof(x_dir); i++)
+        for (int i = 0; i < SIZE; i++)
         {
             float x_abs = abs(x_dir[i]);
             float y_abs = abs(y_dir[i]);
@@ -38,31 +47,37 @@ public:
         return z_dir;
     }
 
-    int calc_steps(float *data,int time)
+    uint8_t calc_steps(float *data, uint16_t time_old)
     {
-        int steps = 0;
-        //Serial.println("Func call");
-        // float *averaged_data = smoothen_signal(data);
+        uint8_t steps = 0;
+        // Serial.println("Func call");
+        //  float *averaged_sdata = smoothen_signal(data);
         float treshold = dynamic_treshold(data);
-        float time1=0;
-        for (int i = 0; i < sizeof(data) - 1; i++)
+        uint16_t time_new = 0;
+
+        for (int i = 0; i < SIZE - 1; i++)
         {
             float sample_old = data[i];
             float sample_new = data[i + 1];
+
             // Add a step when the new data is smaller than old and compared to treshold
-            if (sample_new < sample_old && treshold < sample_old && sample_new < treshold) {
-              //Serial.println("step");
-              time1 = i*time -time1;
-              if(time1 > 20 && time1 < 2000 ) { //TO -DO NAPAKA PRI ZAZNAVI ZARADI OKEN UPOŠTEVAJ |----.|.----| KJER JE | OKNO, . KORAK, - NIC
+            if (sample_new < sample_old && treshold < sample_old && sample_new < treshold)
+            {
+                uint16_t time_temp = i * time_old - time_new;
+                // Serial.println("step");
                 steps++;
-              }
+                if (time_new > 200)
+                { // TO -DO NAPAKA PRI ZAZNAVI ZARADI OKEN UPOŠTEVAJ |----.|.----| KJER JE | OKNO, . KORAK, - NIC
+
+                    time_new = time_temp;
+                }
             }
         }
 
         return steps;
     }
 
-    float calc_speed(int steps_per_2s)
+    float calc_speed(uint16_t steps_per_2s)
     {
         // Interval is 0.04 s
         float stride;
@@ -101,27 +116,54 @@ private:
     {
         float treshold;
         float average;
-        int size = sizeof(averaged_data);
 
-        for (int i = 0; i < size; i++)
+        for (int i = 0; i < SIZE; i++)
             average += averaged_data[i];
 
-        average /= size;
+        average /= SIZE;
+
+        return average;
+    }
+    float dynamic_treshold_MIN_MAX(float *averaged_data)
+    {
+        float treshold;
+
+        float min = FLT_MAX;
+        float max = FLT_MIN;
+        for (int i = 0; i < SIZE; i++)
+        {
+
+            if (averaged_data[i] < min)
+            {
+                min = averaged_data[i];
+            }
+            if (averaged_data[i] > max)
+            {
+                max = averaged_data[i];
+            }
+        }
+        float average = (min + max) / 2;
 
         return average;
     }
 
-    float *smoothen_data(float *data)
+    float *smooth_data(float *data)
     {
-        float averaged_data[sizeof(data) / 3 + 1];
+        // Upoštevaj manjšo velikost od SIZE !!!!!!!
+        float averaged_data[SIZE - 2];
+        int size = SIZE - 2; // sizeof(averaged_data);
 
-        for (int i = 0; i < sizeof(averaged_data); i += 3)
-            averaged_data[i] = (data[i] + data[i + 1] + data[i + 2]) / 3;
+        averaged_data[0] = (data[0] + data[1]) / 2; // padding na zacetku in koncu, da ohranimo dolzino signala
+        averaged_data[0] = (data[SIZE - 2] + data[SIZE - 1]) / 2;
+        for (int i = 1; i < size - 1; i++)
+        {
+            averaged_data[i] = (data[i - 1] + data[i] + data[i + 1]) / 3; // filter signala - povprečje prejsnje, zdasnje naslednj
+        }
 
         return averaged_data;
     }
 
-    int filter_steps(int steps, float *intervals)
+    uint16_t filter_steps(uint16_t steps, float *intervals)
     {
         // Filter data if the interval is less than 0.2s and more than 2s
         // intervals je tabela casov med 2 korakoma
--- main/main.ino
+++ main/main.ino
@@ -1,6 +1,9 @@
 #include <Wire.h>
 #include <Ticker.h>
 #include "equation.h"
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include "password.h"
 
 // <-------------------- I2C  funkciji zacetek -------------------------->
 void I2CWriteRegister(uint8_t I2CDevice, uint8_t RegAdress, uint8_t Value)
@@ -43,27 +46,28 @@ void I2CReadRegister(uint8_t I2CDevice, uint8_t RegAdress, uint8_t NBytes, uint8
 #define MPU_ADD 104
 // Naslov registra za pospesek
 #define ACC_MEAS_REG 59
-#define delilnik 16384
-#define RATE 25
+#define delilnik 16384.0f
+#define RATE 50
 // Stevilo uzorcev za kalibracijo
 #define CAL_NO 100
-// Stevilo uzorcev za branje
+// Stevilo vzorcev za branje
 #define READ_NO 10
 // Total array length
 #define TOTAL_COUNT 50
-//CASOVNI KORAK BELEZENJA
+// CASOVNI KORAK BELEZENJA
 #define TIME_STEP 40
 uint8_t LED_niz[4] = {0xfe, 0xfd, 0xfb, 0xf7}; // led diode
 uint8_t LED_niz1[2] = {0xfe, 0xff};
 #define I2C_ADD_IO1 32
 
 // Count to 50, then reset
-uint32_t write_count = 0;
-uint8_t totalSteps = 0;
+uint8_t write_count = 0;
+uint16_t totalSteps = 0;
 float x_acc_array[TOTAL_COUNT];
 float y_acc_array[TOTAL_COUNT];
 float z_acc_array[TOTAL_COUNT];
 
+BlynkTimer timer;
 Ticker tick, tickLED, readSensor;
 Equation eq;
 // Globalni stevec zanke
@@ -72,21 +76,23 @@ uint8_t iter = 0;
 // Meritve iz senzorja
 uint8_t accMeas[] = {0, 0, 0, 0, 0, 0};
 
-// Kalibracione vrednosti
+// Kalibracijske vrednosti
 float accX_off = 0;
 float accY_off = 0;
 float accZ_off = 0;
 // Meritve  acc
-float accX, accY;
-float accZ = -1.00;
+float accX = 0;
+float accY = 0;
+float accZ = 0;
+
+u_int32_t counter = 0;
 
 void MPU9250_init()
 {
-  // Resetiraj MPU9250 senzora => Register PWR_MGMT_1 (107)
+  // Resetiraj MPU9250 senzor => Register PWR_MGMT_1 (107)
   I2CWriteRegister(MPU_ADD, 107, 128); // 128 = 1000 0000
   // Pocakaj
   delay(500);
-  // Preveri ID od senzora => Register WHO_AM_I (117)
   uint8_t ID;
   I2CReadRegister(MPU_ADD, 117, 1, &ID);
   Serial.println("ID:");
@@ -94,9 +100,6 @@ void MPU9250_init()
   // 4 in 3 bit dolocata obseg
   I2CWriteRegister(MPU_ADD, 27, 0); //
   delay(100);
-  // Accelerator Conf => Register ACCEL_CONFIG (28)
-  // 4 in 3 bit dolocata obseg
-  // Opciono => Register ACCEL_CONFIG_2 (29)
   I2CWriteRegister(MPU_ADD, 28, 0); //
   delay(100);
 }
@@ -124,9 +127,7 @@ void calibrateACC()
 
   delay(1000);
 
-  int samp = CAL_NO;
   int32_t table;
-  Serial.println("Kalibracija ziroskopa");
 
   for (int i = 0; i < CAL_NO; i++)
   {
@@ -139,15 +140,15 @@ void calibrateACC()
     table = (int8_t)Wire.read();
     table = table << 8;
     table += (uint8_t)Wire.read();
-    accX_off += (table / delilnik) / RATE;
+    accX_off += (table / delilnik);
     table = (int8_t)Wire.read();
     table = table << 8;
     table += (uint8_t)Wire.read();
-    accY_off += (table / delilnik) / RATE;
+    accY_off += (table / delilnik);
     table = (int8_t)Wire.read();
     table = table << 8;
     table += (uint8_t)Wire.read();
-    accZ_off += (table / delilnik) / RATE;
+    accZ_off += (table / delilnik);
 
     delay(1000 / RATE);
   }
@@ -157,11 +158,11 @@ void calibrateACC()
   accY_off /= CAL_NO;
   accZ_off /= CAL_NO;
 
-  Serial.println("pospeskomer X osa");
+  Serial.println("pospeskomer v X osi");
   Serial.println(accX_off);
-  Serial.println("pospeskomer Y osa");
+  Serial.println("pospeskomer v Y osi");
   Serial.println(accY_off);
-  Serial.println("pospeskomer Z osa");
+  Serial.println("pospeskomer v Z osi");
   Serial.println(accZ_off);
 }
 void readACC()
@@ -184,62 +185,87 @@ void readACC()
   table = (int8_t)Wire.read();
   table = table << 8;
   table += (uint8_t)Wire.read();
-  // Da se izognemo meritvenemu hrupu
-  // računamo povprečno vrednost na podlagi RATE uzorcev
-  // RATE - število uzorcev
-  // acc_x(i) =  (table(i) / 131.0f) - acc_x_calibrated
-  // acc_x = sum_{i=0}^{RATE-1}(acc_x(i)/RATE) -> LATEX
-  accX += ((table / delilnik) - accX_off) / RATE;
+  accX = ((table / delilnik) - accX_off);
 
   table = (int8_t)Wire.read();
   table = table << 8;
   table += (uint8_t)Wire.read();
-  accY += ((table / delilnik) - accY_off) / RATE;
+  accY = ((table / delilnik) - accY_off);
 
   table = (int8_t)Wire.read();
   table = table << 8;
   table += (uint8_t)Wire.read();
-  accZ += ((table / delilnik) - accZ_off) / RATE;
+  accZ = ((table / delilnik) - accZ_off);
 
-  if (count % RATE == 0)
-  {
-    // Izpišemo
-    Serial.print("ACC_X: X= ");
-    Serial.print(accX);
-    Serial.println("\n");
-    Serial.print("ACC_Y: Y= ");
-    Serial.print(accY);
-    Serial.println("\n");
-    Serial.print("ACC_Z: Z= ");
-    Serial.print(accZ);
-    Serial.println("\n");
-    // resetiramo vrednost
-    accX = 0;
-    accY = 0;
-    accZ = -1;
-  }
+  /*
 
-  if (write_count == 50)
+    Kliči funkcije iz equation.h
+
+  */
+
+  if (write_count == TOTAL_COUNT)
   {
     // Obdelaj podatke
     float *highest_acc = eq.get_highest_acc(x_acc_array, y_acc_array, z_acc_array);
-    int steps = eq.calc_steps(highest_acc,TIME_STEP);
-    totalSteps+=steps;
+
+    // for (int i = 0; i < TOTAL_COUNT; i++)
+    //   printf("%f ", highest_acc[i]);
+    // Serial.println("\n");
+
+    uint8_t steps = eq.calc_steps(highest_acc, TIME_STEP);
+    totalSteps += steps;
     float avg_speed = eq.calc_speed(steps);
-     Serial.println("....................................................");
-     Serial.println(totalSteps);
-     write_count = 0;
+    Serial.println("....................................................");
+    Serial.println(totalSteps);
+    Serial.println("Counter:");
+    Serial.println(counter);
+
+    write_count = 0;
+    // Izpišemo
+    Serial.print("ACC_X: X= ");
+    Serial.print(accX * 9.81);
+    Serial.println("\n");
+    Serial.print("ACC_Y: Y= ");
+    Serial.print(accY * 9.81);
+    Serial.println("\n");
+    Serial.print("ACC_Z: Z= ");
+    Serial.print(accZ * 9.81);
+    Serial.println("\n");
+    accX = 0;
+    // resetiramo vrednost
+    // accX = 0;
+    // accY = 0;
+    // accZ = 0;
   }
+
+  /*
+
+    Konec if stavka
+
+  */
+
   // Dodaj trenutni count v array
+  // Serial.printf("write c %d\n", write_count);
   x_acc_array[write_count] = accX;
   y_acc_array[write_count] = accY;
   z_acc_array[write_count] = accZ;
-  
 
   // števec
-  count = count + 1;
+  counter++;
   write_count++;
   digitalWrite(PIN_LED, 1);
+}
+
+void send()
+{
+  float razdalja = 10;
+  float kalorije = 1000;
+  char Tip[] = "Hoja";
+
+  Blynk.virtualWrite(V0, totalSteps);
+  Blynk.virtualWrite(V1, razdalja);
+  Blynk.virtualWrite(V2, kalorije);
+  Blynk.virtualWrite(V3, Tip);
 }
 
 void setup()
@@ -247,6 +273,7 @@ void setup()
   Serial.println("Zacetek");
   // Serijska komunikacija
   Serial.begin(115200);
+
   // I2C
   Wire.begin(12, 14);
   // SDA - 12 pin
@@ -255,6 +282,10 @@ void setup()
   // Podesavanje senzorja
   // https://github.com/bolderflight/mpu9250/blob/main/src/mpu9250.cpp
   MPU9250_init();
+
+  // Določi velikost arraya za interval pospeška pri korakih
+  eq.setSize(TOTAL_COUNT);
+
   // Kalibracija
   Serial.println("Kalibracijo pospeskometra zacenjam cez 5 sec.");
   Serial.println("Prosim postavite napravo na ravnino in jo pustite na miru.");
@@ -268,10 +299,15 @@ void setup()
   // Branje senzorja
   tickLED.detach();
   tickLED.attach_ms(400, utripLED1);
-  tick.attach_ms(40, readACC);
+  tick.attach_ms(TIME_STEP, readACC);
+
+  Blynk.begin(BLYNK_AUTH_TOKEN, SSID, PASSWORD);
+
+  timer.setInterval(200L, send);
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
+  Blynk.run();
+  timer.run();
 }
