339d39392aeaf0eac7c4e95c30c1e5d333a6d513
Mt <matjaz.zupanic1@gmail.com>
2023-05-21 22:59:12 +0200

    Dodajanje navodil

--- main/constants.h
+++ main/constants.h
@@ -1,7 +1,3 @@
-#define HEIGHT 1.80
-#define WEIGHT 80
-#define PIN_LED 2
-
 // Naslov MPU9250 na I2C vodilu
 #define MPU_ADD 104
 #define I2C_ADD_IO1 32
@@ -11,20 +7,30 @@
 
 #define delilnik 16384.0f
 
+#define PIN_LED 2
+
+// NASTAVLJIVI PARAMETRI SO SPODAJ -PO potrebi lahko spreminjate!
+
 // Število vzorcev za kalibracijo
 #define CAL_NO 100
 
-// Časovni korak beleženja v ms
+// višina uporabnika
+#define HEIGHT 1.80
+
+//teža uporabnika
+#define WEIGHT 80
+
+// Časovni korak beleženja v ms - 1/frekvenca
 #define TIME_STEP 40
 
 //Prag pri računanju korakov z odvodi, da se izognemo false positives
 #define DERIVATIVE_TRESHOLD 0.1f
 
-// Število beleženj preden se podatki pošljejo na Blynk
+// Število beleženj preden se podatki obdelajo (izračun korakov) in pošljejo na Blynk
 #define SIZE 50
 
-//širina okna za filter
+//širina okna za moving avrage filter - izberi sode številke, default=2
 #define WINDOWS_SIZE 2
 
-// Spodnja meja beleženja v ms
+// Spodnja meja zaznave v ms, minimalni čas med zaznavo koraka
 #define MIN_TIME_STEP 200
---
+++ main/password_change.h
@@ -0,0 +1,7 @@
+// spremeni ime dokumenta v passwords.h, ter med narekovaje vpiši ustrezne podatke
+#define BLYNK_TEMPLATE_ID "VSTAVI BLINK ID"
+#define BLYNK_TEMPLATE_NAME "VSTAVI IME"
+#define BLYNK_AUTH_TOKEN "TOKEN BLINKA"
+
+#define SSID "IME WIFIJA"
+#define PASSWORD "GESLO WIFIJA"
